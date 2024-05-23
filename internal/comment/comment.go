352a2a96ac66ca6e72eca13c068a69c51735daf9
afshin990karimi <afshin990karimi@gmail.com>
2024-05-23 09:48:45 +0330

    till the end of 6

---
+++ internal/comment/comment.go
@@ -0,0 +1,39 @@
+package comment
+
+import (
+	"context"
+	"fmt"
+)
+
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+// Service - is the struct on which all our
+// logic will be built on top of
+type Service struct {
+	Store Store
+}
+
+// NewService - returns a pointer to a new service
+func NewService(Store Store) *Service {
+	return &Service{}
+}
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("retrieving a comment")
+	cmt, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, err
+	}
+
+	return cmt, nil
+}
