58fccdeba13cdf3a106ec5bc17fe78f23562c2c2
afshin990karimi <afshin990karimi@gmail.com>
2024-05-23 11:57:25 +0330

    add Dockerfile

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -35,6 +35,7 @@ func NewService(Store Store) *Service {
 
 func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
 	fmt.Println("retrieving a comment")
+
 	cmt, err := s.Store.GetComment(ctx, id)
 	if err != nil {
 		fmt.Println(err)
